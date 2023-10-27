f1517dc0e22c1a4ce357c0da77f793cfdcf2d9e6
Paulo Silva <pauloeddias@gmail.com>
2023-10-27 15:05:34 -0300

    adgadfg

--- lambda_functions/surelogix/surelogix_function_temp.py
+++ lambda_functions/surelogix/surelogix_function_temp.py
@@ -212,7 +212,7 @@ def lambda_handler(event, context=None):
                 'sub_customer': doc_type.split('-')[-2],
             },
             'model_info': model_info,
-            'file_type':"blfasdfp",
+            'file_type':"blffaasdfp",
             }
             
     except UnintendedFileException as e:
